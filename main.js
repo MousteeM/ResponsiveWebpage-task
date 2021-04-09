370727cc9a8a47ea52800e5418e48248e56b8d01
Mustapha <moustee01@gmail.com>
2021-04-09 20:44:40 +0200

    task submission

---
+++ main.js
@@ -0,0 +1,13 @@
+// const hamburger = document.querySelector(".hamburger");
+// const navLinks = document.querySelector(".nav-links");
+// const links = document.querySelectorAll(".nav-links li");
+
+
+// hamburger.addEventListener("click", () => {
+//     navLinks.classList.toggle ("open");
+// } );
+
+// // function mobileMenu() {
+// //     hamburger.classList.toggle("active");
+// //     navMenu.classList.toggle("active");
+// // }
